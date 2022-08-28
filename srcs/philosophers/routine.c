2d45ce24528d7f7747c35d4b8672070a77cfe13f
mychibani <m.yassinechibani@gmail.com>
2022-08-28 21:37:51 +0200

    working but not opt

--- srcs/philosophers/routine.c
+++ srcs/philosophers/routine.c
@@ -41,7 +41,7 @@ void	modifier_death(t_philo *philo, int res)
 	if (res == -1)
 		philo->global->die = -1;
 	if (res == 1 && philo->global->die != -1)
-		philo->global->die = 1;
+		philo->global->die += 1;
 	pthread_mutex_unlock(&philo->global->mutex_ressources);
 }
 
@@ -54,7 +54,6 @@ void	sleeping_time(t_philo *philo)
 	if (exact_time() > philo->end)
 		modifier_death(philo, -1);
 	state_printer(philo->id, IS_THINKING, philo);
-	__usleep(1, philo);
 	eating_time(philo);
 }
 
@@ -79,8 +78,8 @@ void	eating_time(t_philo *philo)
 		philo->end = exact_time() + philo->philo_info.time_to_die;
 	}
 	drop_forks(philo);
-	// if (philo->meal != philo->philo_info.to_feed)
-	// 	modifier_death(philo, 1);
+	if (philo->meal != philo->philo_info.to_feed)
+		modifier_death(philo, 1);
 	if (nobody_died(philo) && philo->meal != philo->philo_info.to_feed)
 		sleeping_time(philo);
 }
